55ea182304473fb491888745ebb3a3a488692f6a
xiongyubin <1730829742@qq.com>
2024-05-28 11:18:28 +0800

    upd

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,19 @@
 <template>
-    2222
+    <!-- <van-field name="radio" label="输入框对齐方式">
+        <template #input>
+            <van-radio-group v-model="form.inputAlign" direction="horizontal">
+                <van-radio name="left">左侧对齐</van-radio>
+                <van-radio name="center">居中</van-radio>
+                <van-radio name="right">右侧对齐</van-radio>
+            </van-radio-group>
+        </template>
+    </van-field> -->
+
 </template>
+
+<script setup>
+import { ref } from 'vue'
+const form = ref({
+    inputAlign: 'left',
+})
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-    <van-form @submit="onSubmit">
-        <van-cell-group inset>
-            <van-field v-model="username" name="用户名" label="用户名" placeholder="用户名"
-                :rules="[{ required: true, message: '请填写用户名' }]" />
-            <van-field v-model="password" type="password" name="密码" label="密码" placeholder="密码"
-                :rules="[{ required: true, message: '请填写密码' }]" />
-        </van-cell-group>
-    </van-form>
+    <van-watermark content="这是" :opacity=".4" />
+    <van-cell-group inset>
+        <van-field v-model="form.price" label="price" type="digit" placeholder="输入价格" input-align="right" />
+        <van-field v-model="purchaseTax" label="price" type="digit" placeholder="根据价格自动计算" input-align="right" />
+        <van-field v-model="form.price" label="price" type="digit" placeholder="输入价格" input-align="right" />
+        <van-field v-model="form.price" label="price" type="digit" placeholder="输入价格" input-align="right" />
+        <van-field v-model="form.price" label="price" type="digit" placeholder="输入价格" input-align="right" />
+
+    </van-cell-group>
 </template>
 
 <script setup>
 import { ref, computed, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
-const val = ref('11122')
+const form = ref({
+    price: 0
+})
+
+const purchaseTax = computed(() => {
+    if (!form.value.price) return ''
+    return form.value.price * 0.1
+})
+
 onMounted(() => {
     console.log(useRoute().query)
 })
