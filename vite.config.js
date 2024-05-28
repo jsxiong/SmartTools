d2b11691ca46661e30dbbdd4fc4dddab3e83732f
xiongyubin <1730829742@qq.com>
2024-05-28 17:58:15 +0800

    upd

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import { VantResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0'
+  },
   plugins: [
     vue(),
     Pages({
