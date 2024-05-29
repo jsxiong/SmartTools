68505b65028ee7353f2ccc15227dddbe71ddc0a7
xiongyubin <1730829742@qq.com>
2024-05-29 17:41:11 +0800

    base

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { VantResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jsxiong/SmartTools',
+  base: '/SmartTools',
   build: {
     outDir: 'dist',
   },
